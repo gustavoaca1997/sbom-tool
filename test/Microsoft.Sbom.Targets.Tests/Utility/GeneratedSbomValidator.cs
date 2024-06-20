c2f2c8e4731110406229f6a9452ae690d4597455
gustavoaca1997 <gustavoaca1997@gmail.com>
2024-06-19 19:27:53 -0700

    Print the content of the parent of the parent folder

--- test/Microsoft.Sbom.Targets.Tests/Utility/GeneratedSbomValidator.cs
+++ test/Microsoft.Sbom.Targets.Tests/Utility/GeneratedSbomValidator.cs
@@ -30,7 +30,7 @@ internal class GeneratedSbomValidator
 
     internal void AssertSbomIsValid(string manifestPath, string buildDropPath, string expectedPackageName, string expectedPackageVersion, string expectedPackageSupplier, string expectedNamespaceUriBase, string expectedNamespaceUriUniquePart = null, string buildComponentPath = null)
     {
-        var parentFolderPath = Path.GetDirectoryName(manifestPath);
+        var parentFolderPath = Path.GetDirectoryName(Path.GetDirectoryName(manifestPath));
         var fileNames = Directory.GetFiles(parentFolderPath).Select(Path.GetFileName);
 
         Assert.IsTrue(File.Exists(manifestPath), $"Manifest file doesn't exist at {manifestPath}. {string.Join(", ", fileNames)}");
