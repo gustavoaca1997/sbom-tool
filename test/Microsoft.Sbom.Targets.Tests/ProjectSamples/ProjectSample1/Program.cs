99fd7a59e811b0ffd32d7c1b4a6229a500a7fac0
gustavoaca1997 <gustavoaca1997@gmail.com>
2024-07-30 20:53:18 -0700

    Start creating E2E/integration tests

---
+++ test/Microsoft.Sbom.Targets.Tests/ProjectSamples/ProjectSample1/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+internal class Program
+{
+    public static void Method(string[] args)
+    {
+        // Copyright (c) Microsoft. All rights reserved.
+        // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+        Console.WriteLine("Hello, World!");
+    }
+}
